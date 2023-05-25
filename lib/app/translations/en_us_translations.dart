1ad5ffd5b3c3fdeab62347cde3f9340cf0d69ea6
quangnd2203 <quangnd.nta@gmail.com>
2023-05-25 13:53:41 +0700

    no message

--- lib/app/translations/en_us_translations.dart
+++ lib/app/translations/en_us_translations.dart
@@ -1,16 +1,3 @@
 final Map<String, String> enUs = <String, String>{
   'splash_getting':'Hello',
-
-  //Dashboard
-  'dash_board_device':'Device',
-  'dash_board_door':'Door',
-  'dash_board_user':'User',
-  //Door
-  'door_group_settings':'Door group settings',
-  'door_group_name':'Door group name',
-  'door_parent_group_setting':'Parent group setting',
-  'door_color':'Color',
-  'door_affiliation_setting':'Affiliation door setting:',
-  'door_current_group':'Current group',
-  'door':'Door',
 };
